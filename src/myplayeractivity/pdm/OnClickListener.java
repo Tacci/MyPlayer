ac499f2c19bb3911359204df582c9d58efcfbe61
Ruggero Visconti <er_mejo85@hotmail.it>
2012-01-19 23:08:56 +0000

    7 esercizio

---
+++ src/myplayeractivity/pdm/OnClickListener.java
@@ -0,0 +1,13 @@
+package myplayeractivity.pdm;
+
+import android.view.View;
+
+public class OnClickListener implements android.view.View.OnClickListener {
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
